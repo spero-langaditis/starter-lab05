c5cbebad588f53bc2b2983a7c005fb1b0a66c30b
spero <slangaditis94@hotmail.com>
2015-10-30 13:27:57 -0700

    Updated the navbar menu for admin, added the admin controller and created an admin list view. As well as handled quote addition. Created a form for quote addition, and added submission and error handling for the quote.

--- application/config/constants.php
+++ application/config/constants.php
@@ -48,5 +48,6 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
+define('BR', '<br/>');
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
