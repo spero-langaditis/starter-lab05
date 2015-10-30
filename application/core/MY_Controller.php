cb8d1913c6a3bbd60c6a8b02a75bf8aa8a696875
spero <slangaditis94@hotmail.com>
2015-10-30 10:42:25 -0700

    downloaded and incorporated the jrating widget and edited all the files with the proper links. also autoloaded caboose and added it in the core controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -37,6 +37,13 @@ class Application extends CI_Controller {
         // finally, build the browser page!
         $this->data['data'] = &$this->data;
         $this->parser->parse('_template', $this->data);
+        // convert caboose output into view parameters
+        $this->data['caboose_styles'] = $this->caboose->styles();
+        $this->data['caboose_scripts'] = $this->caboose->scripts();
+        $this->data['caboose_trailings'] = $this->caboose->trailings();
+
+
+        
     }
 
 }
